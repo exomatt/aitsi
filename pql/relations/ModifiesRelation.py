7b19cb5a71ae1e24ec40cb6accd65afdc569d858
exomat <exomat@wp.pl>
2020-03-21 01:36:27 +0100

    [#25][exomat][Update modifiesrelation -> teraz uzywa mod_table i var_table, dodanie dodatkowego warunka do modifiestable kiedy zmienna o danej nazwie moze nie istniec w tabeli ]

--- pql/relations/ModifiesRelation.py
+++ pql/relations/ModifiesRelation.py
@@ -1,149 +1,74 @@
-from typing import Union, List, Optional, Set
+from typing import Union, List, Set
 
+from aitsi_parser.ModifiesTable import ModifiesTable
+from aitsi_parser.VarTable import VarTable
 from pql.Node import Node
 from pql.utils.SearchUtils import SearchUtils
 
 
 class ModifiesRelation:
-    def __init__(self, ast_tree: Node) -> None:
+    def __init__(self, ast_tree: Node, modifies_table: ModifiesTable, var_table: VarTable) -> None:
         super().__init__()
         self.ast_tree: Node = ast_tree
+        self.modifies_table: ModifiesTable = modifies_table
+        self.var_table: VarTable = var_table
 
-    def modifies(self, param_first: str, param_second: str, ) -> Union[List[str], List[int]]:
-        search_utils: SearchUtils = SearchUtils(self.ast_tree)
+    def modifies(self, param_first: str, param_second: str) -> Union[bool, List[str], List[int]]:
         if param_first.isdigit():
-            if param_second == '_':
-                # p1 jest liczba, a p2 zmienna wild card  np. '_' zwraca nazwe zmiennej modyfikowanej w tej lini w liscie lub pusta liste
-                node: Optional[Node] = search_utils.find_node_by_line(int(param_first))
-                if node is None:
-                    return []
-                elif node.node_type == 'ASSIGN':
-                    for children in node.children:
-                        if children.node_type == 'NAME':
-                            return [children.line]
-                return []
-            elif param_second == 'VARIABLE':
-                # p1 jest liczba, a p2 zmienna variable v czyli   np. 'VARIABLE' Modifies('3',v) zwraca nazwe zmiennej modyfikowanej w tej lini w liscie lub pusta liste
-                node: Optional[Node] = search_utils.find_node_by_line(int(param_first))
-                if node is None:
-                    return []
-                elif node.node_type == 'ASSIGN':
-                    for children in node.children:
-                        if children.node_type == 'NAME':
-                            return [children.value]
-                return []
+            if param_second == 'VARIABLE':
+                # p1 jest liczba, a p2 zmienna variable v czyli   np. 'VARIABLE' Modifies('3',v) zwraca liste ze zmiennymi modyfikowanymi tam lub pusta liste
+                return self._digit_and_wild_card_or_variable(param_first)
+            elif param_second == '_':
+                # p1 jest liczba, a p2 zmienna wild card  np. '_' zwraca liste ze zmiennymi modyfikowanymi tam lub pusta liste
+                return self._digit_and_wild_card_or_variable(param_first)
             else:
-                # p1 jest liczba, a p2 zmienna variable np. 'x'
-                # zwraca numer lini  jezeli w danej linii zachodzi modyfikacja zmiennej w innym przypadku []
-                node: Optional[Node] = search_utils.find_node_by_line(int(param_first))
-                if node is None:
-                    return []
-                elif node.node_type == 'ASSIGN':
-                    for children in node.children:
-                        if children.node_type == 'NAME':
-                            if children.value == param_second:
-                                return [children.line]
-                return []
-
+                # p1 jest liczba, a p2 zmienna variable np. 'x' zwraca True jezeli w danej lini jest zmienna modyfikowana
+                return self.modifies_table.is_modified(param_second, int(param_first))
         elif param_first == '_':
             if param_second == '_':
-                # p1 jest '_', a p2 zmienna variable np.  variable v Modifies('_', '_') zwraca numery lini  gdzie jest cos modyfikowane
-                stmt_list = self._get_all_stmt_list_in_procedure(search_utils)
-                result_lines: List[int] = []
-                for stmt in stmt_list:
-                    if stmt.node_type == 'ASSIGN':
-                        for children in stmt.children:
-                            if children.node_type == 'NAME':
-                                result_lines.append(children.line)
-                                break
-
-                return result_lines
-                pass
+                # p1 jest '_', a p2 zmienna variable np.  variable v Modifies('_', '_')
+                return self.var_table.table['variable_name'].tolist()
             elif param_second == 'VARIABLE':
                 # p1 jest '_', a p2 zmienna variable np.  variable v Modifies('_', v) zwraca nazwy zmiennych   ktore sa modyfikowane gdziekolwiek
-                nodes_by_type: List[Node] = []
-                nodes_by_type.extend(search_utils.find_node_by_type('IF'))
-                nodes_by_type.extend(search_utils.find_node_by_type('WHILE'))
-                nodes_by_type.extend(search_utils.find_node_by_type('PROCEDURE'))
-                stmt_list: List[Node] = []
-                return self._get_all_modified_variable_in_stmtlist(nodes_by_type, stmt_list)
+                return self.var_table.table['variable_name'].tolist()
             else:
                 # p1 jest '_', a p2 zmienna variable np. 'x' zwraca numery lini    gdzie jest modyfikowanea zmienna o nazwie x
-                stmt_list = self._get_all_stmt_list_in_procedure(search_utils)
-                result_lines: List[int] = []
-                for stmt in stmt_list:
-                    if stmt.node_type == 'ASSIGN':
-                        for children in stmt.children:
-                            if children.node_type == 'NAME':
-                                if children.value.strip() == param_second:
-                                    result_lines.append(children.line)
-                                break
-                return result_lines
+                return self.modifies_table.get_modifies(param_second)
         else:
             if param_second == '_':
                 # p1 jest stmt np 'IF', a p2  '_'   przypadek skrajny Modifies('IF',_) zwraca linie gdzie cokolwiek jest modyfikowane
-                stmt_list = self._get_stmt_list_by_param(param_first, search_utils)
-                result_lines: List[int] = []
-                for stmt in stmt_list:
-                    if stmt.node_type == 'ASSIGN':
-                        for children in stmt.children:
-                            if children.node_type == 'NAME':
-                                result_lines.append(children.line)
-                            break
-                return result_lines
+                return self._str_type_and_wild_card(param_first)
             elif param_second == 'VARIABLE':
                 # wedlug handbooka to kiedy jest if i while to zwracamy wszystkie modyfikowane  zmienne
                 # np. 'VARIABLE' Modifies('IF',v)
-                nodes_by_type: List[Node] = []
-                nodes_by_type.extend(search_utils.find_node_by_type(param_first))
-                stmt_list: List[Node] = []
-                return self._get_all_modified_variable_in_stmtlist(nodes_by_type, stmt_list)
+                return self._str_type_and_wild_card(param_first)
             else:
-                # p1 jest stmt np 'IF', a p2 zmienna variable np. 'x' zwracamy numer lini modyfikacji zmiennej 'x'
-                stmt_list = self._get_stmt_list_by_param(param_first, search_utils)
-                result_lines: List[int] = []
-                for stmt in stmt_list:
-                    if stmt.node_type == 'ASSIGN':
-                        for children in stmt.children:
-                            if children.node_type == 'NAME':
-                                if children.value.strip() == param_second:
-                                    result_lines.append(children.line)
-                                break
-                return result_lines
+                # p1 jest stmt np 'IF', a p2 zmienna variable np. 'x' zwracamy numer lini tego ifa
+                return self._str_with_type_and_variable_by_name(param_first, param_second)
 
-    def _get_stmt_list_by_param(self, param_first, search_utils) -> List[Node]:
-        nodes_by_type: List[Node] = []
-        nodes_by_type.extend(search_utils.find_node_by_type(param_first))
-        stmt_list: List[Node] = []
-        for node in nodes_by_type:
-            for children in node.children:
-                if children.node_type == 'STMT_LIST':
-                    stmt_list.extend(children.children)
-        return stmt_list
+    def _str_with_type_and_variable_by_name(self, param_first, param_second):
+        search_utils: SearchUtils = SearchUtils(self.ast_tree)
+        result: List[int] = []
+        lines_numbers: List[int] = []
+        lines_numbers.extend(search_utils.find_node_line_number_by_type(param_first))
+        for number in lines_numbers:
+            if self.modifies_table.is_modified(param_second, number):
+                result.append(number)
+        return result
 
-    def _get_all_modified_variable_in_stmtlist(self, nodes_by_type, stmt_list) -> List[str]:
-        result_lines: Set[str] = set()
-        for node in nodes_by_type:
-            for children in node.children:
-                if children.node_type == 'STMT_LIST':
-                    stmt_list.extend(children.children)
-        for stmt in stmt_list:
-            if stmt.node_type == 'ASSIGN':
-                for children in stmt.children:
-                    if children.node_type == 'NAME':
-                        result_lines.add(children.value.strip())
-                    break
-        return list(result_lines)
+    def _str_type_and_wild_card(self, param_first):
+        search_utils: SearchUtils = SearchUtils(self.ast_tree)
+        result: Set[str] = set()
+        lines_numbers: List[int] = []
+        lines_numbers.extend(search_utils.find_node_line_number_by_type(param_first))
+        for number in lines_numbers:
+            result.update(self.modifies_table.get_modified(number))
+        return list(result)
 
-    def _get_all_stmt_list_in_procedure(self, search_utils) -> List[Node]:
-        nodes_by_type: List[Node] = []
-        nodes_by_type.extend(search_utils.find_node_by_type('IF'))
-        nodes_by_type.extend(search_utils.find_node_by_type('WHILE'))
-        nodes_by_type.extend(search_utils.find_node_by_type('PROCEDURE'))
-        stmt_list: List[Node] = []
-
-        for node in nodes_by_type:
-            for children in node.children:
-                if children.node_type == 'STMT_LIST':
-                    stmt_list.extend(children.children)
-        return stmt_list
+    def _digit_and_wild_card_or_variable(self, param_first):
+        result: List[str] = []
+        variables: List[str] = self.var_table.table['variable_name'].tolist()
+        for variable in variables:
+            if self.modifies_table.is_modified(variable, int(param_first)):
+                result.append(variable)
+        return result
