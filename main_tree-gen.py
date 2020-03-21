aa06a8648853f65c4941f92f78eeb0ca1f4e70f8
exomat <exomat@wp.pl>
2020-03-21 15:12:56 +0100

    [#tree gen][exomat][Concat do wartości noda przy tworzeniu drzewa ]

--- main_tree-gen.py
+++ main_tree-gen.py
@@ -1,21 +1,31 @@
-from anytree.importer import DictImporter
-from anytree import RenderTree
-from anytree.exporter import UniqueDotExporter
 import json
+
+from anytree.exporter import UniqueDotExporter
+from anytree.importer import DictImporter
+
 importer = DictImporter()
-with open('AST.json') as json_file:
+filename = 'AST.json'
+with open(filename) as json_file:
     data = json.load(json_file)
+
 
 def remove_dots(obj):
     for key in obj.keys():
-        new_key = key.replace("node_type","name")
+        new_key = key.replace("node_type", "name")
         if new_key != key:
-            obj[new_key] = obj[key]
+            if obj['value'].strip() in ['{', '}', '']:
+                obj[new_key] = "Node_type: " + obj[key] + "\n" + "Line: " + str(
+                    obj['line'])
+            else:
+                obj[new_key] = "Node_type: " + obj[key] + "\n" + "Value: " + obj['value'] + "\n" + "Line: " + str(
+                    obj['line'])
             del obj[key]
     return obj
 
-new_json = json.loads(json.dumps(data), object_hook=remove_dots) 
+
+new_json = json.loads(json.dumps(data), object_hook=remove_dots)
 
 root = importer.import_(new_json)
 
-UniqueDotExporter(root).to_picture("tree.png")
+out_filename = "tree.png"
+UniqueDotExporter(root).to_picture(out_filename)
